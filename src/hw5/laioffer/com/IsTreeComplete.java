b2b3c466ebfdb2dfd8b7f12c9da48f810df8b018
hu.993 <senyanghu@gmail.com>
2017-08-30 21:13:07 -0400

    feat: add class files

--- src/hw5/laioffer/com/IsTreeComplete.java
+++ src/hw5/laioffer/com/IsTreeComplete.java
@@ -1,5 +1,39 @@
 package hw5.laioffer.com;
 
+import java.util.*;
+
+import hw4.laioffer.com.TreeNode;
+
 public class IsTreeComplete {
-	
+	public boolean isComplete(TreeNode root) {
+		if (root == null) {
+			return true;
+		}
+		Queue<TreeNode> queue = new LinkedList<>();
+		boolean nonFullNodeSeen = false;
+
+		queue.add(root);
+		while (!queue.isEmpty()) {
+			TreeNode cur = queue.poll();
+			if (cur.left == null) {
+				nonFullNodeSeen = true;
+			} else if (nonFullNodeSeen) {
+				// current node has left child and already meet the non-full node
+				return false;
+			} else {
+				queue.offer(cur.left);
+			}
+
+			if (cur.right == null) {
+				nonFullNodeSeen = true;
+			} else if (nonFullNodeSeen) {
+				// current node has right child and already meet the non-full node
+				return false;
+			} else {
+				queue.offer(cur.right);
+			}
+		}
+
+		return true;
+	}
 }
