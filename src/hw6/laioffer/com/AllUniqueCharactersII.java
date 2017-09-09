c6cce7d9c288b91d3858a6fdebcd757ec314d912
hu.993 <senyanghu@gmail.com>
2017-09-08 21:12:55 -0400

    feat: finish hw7 dfs (permutation problem)

--- src/hw6/laioffer/com/AllUniqueCharactersII.java
+++ src/hw6/laioffer/com/AllUniqueCharactersII.java
@@ -2,6 +2,6 @@ package hw6.laioffer.com;
 
 public class AllUniqueCharactersII {
     public boolean allUnique(String word) {
-        return false;
+        return true;
     }
 }
---
+++ src/hw7/laioffer/com/AllPermutations.java
@@ -0,0 +1,33 @@
+package hw7.laioffer.com;
+
+import java.util.*;
+
+public class AllPermutations {
+	public List<String> permutations(String set) {
+		List<String> res = new ArrayList<>();
+		if (set == null) {
+			return res;
+		}
+		char[] arraySet = set.toCharArray();
+		dfs(arraySet, 0, res);
+		return res;
+	}
+
+	private void dfs(char[] arraySet, int index, List<String> res) {
+		if (index == arraySet.length) {
+			res.add(new String(arraySet));
+			return;
+		}
+		for (int i = index; i < arraySet.length; i++) {
+			swap(arraySet, index, i);
+			dfs(arraySet, index + 1, res);
+			swap(arraySet, i, index);
+		}
+	}
+
+	private void swap(char[] arraySet, int i, int j) {
+		char temp = arraySet[i];
+		arraySet[i] = arraySet[j];
+		arraySet[j] = temp;
+	}
+}
