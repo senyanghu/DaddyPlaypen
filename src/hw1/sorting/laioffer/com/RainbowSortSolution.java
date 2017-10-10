95f9682d2730b20e2eeba8fe39aa1fdadef3cb1a
hu.993 <senyanghu@gmail.com>
2017-10-09 21:40:37 -0400

    fix: rearrange some files and directories.

---
+++ src/hw1/sorting/laioffer/com/RainbowSortSolution.java
@@ -0,0 +1,49 @@
+package hw1.sorting.laioffer.com;
+
+public class RainbowSortSolution {
+	public int[] rainbowSort(int[] array) {
+		if (array == null || array.length <= 1) {
+			return array;
+		}
+		rainbowSortHelper(array, 0, 0, array.length - 1);
+		return array;
+	}
+
+	/**
+	 * 
+	 * @param array
+	 *            the input array
+	 * @param left
+	 *            [0. left) should be all -1
+	 * @param cur
+	 *            current cursor
+	 * @param right
+	 *            (right, length - 1] should be all 1
+	 */
+	private void rainbowSortHelper(int[] array, int left, int cur, int right) {
+		while (cur <= right) {
+			if (array[cur] == -1) {
+				swap(array, left++, cur);
+			} else if (array[cur] == 0) {
+				cur++;
+			} else { // array[cur] == -1
+				swap(array, cur, right--);
+			}
+		}
+	}
+
+	private void swap(int[] array, int i, int j) {
+		int temp = array[i];
+		array[i] = array[j];
+		array[j] = temp;
+	}
+
+	public static void main(String args[]) {
+		RainbowSortSolution rss = new RainbowSortSolution();
+		int[] array = { 0, -1, 1, 1, -1, 0, -1 };
+		rss.rainbowSort(array);
+		for (int element : array) {
+			System.out.println(element);
+		}
+	}
+}
---
+++ src/hw12/sampling/laioffer/com/ReservoirSampling.java
@@ -0,0 +1,28 @@
+package hw12.sampling.laioffer.com;
+
+/**
+ * Consider an unlimited flow of data elements. How do you sample one element
+ * from this flow, such that at any point during the processing of the flow, you
+ * can return a random element from the n elements read so far.
+ * 
+ * You will implement two methods for a sampling class: read(int value) - read
+ * one number from the flow sample() - return at any time the sample, if n
+ * values have been read, the probability of returning any one of the n values
+ * is 1/n, return null (Java)/INT_MIN(C++) if there is no value read so far You
+ * may need to add more fields for the class.
+ */
+public class ReservoirSampling {
+	private int count;
+	private Integer sample;
+
+	public ReservoirSampling() {
+		this.count = 0;
+		this.sample = 0;
+	}
+	
+	
+	
+	public Integer sample() {
+		return sample;
+	}
+}
