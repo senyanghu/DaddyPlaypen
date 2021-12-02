44235a57bfbc9e5bd62fed4d85f797d04acd37f9
Alex Hu <senyanghu@gmail.com>
2021-12-01 23:14:28 -0800

    V2 of selection sort

--- src/com/laioffer/hw01/sort/SelectionSortSolution.java
+++ src/com/laioffer/hw01/sort/SelectionSortSolution.java
@@ -30,10 +30,35 @@ public class SelectionSortSolution {
 	public static void main(String[] args) {
 		SelectionSortSolution s = new SelectionSortSolution();
 		int[] array = { 3, 2, 1, -4 };
-		int[] result = s.sort(array);
+		//int[] result = s.sort(array);
+		int[] result = s.sort_version2(array);
 		for (int i : result) {
 			System.out.println(i);
 		}
+	}
+
+
+	private static int[] sort_version2(int[] arr) {
+		if (arr == null || arr.length == 0 || arr.length == 1) {
+			return arr;
+		}
+
+		// how many iterations
+		for (int i = 0; i < arr.length - 1; i++) {
+			int globalMinIndex = i;
+			// find the global min in the rest of array
+			for (int j = i + 1; j < arr.length; j++) {
+				if (arr[j] < arr[globalMinIndex]) {
+					globalMinIndex = j;
+				}
+			}
+
+			int temp = arr[i];
+			arr[i] = arr[globalMinIndex];
+			arr[globalMinIndex] = temp;
+		}
+
+		return arr;
 	}
 
 }
